3e441820da3fae747bec7ee895e646d511703f73
TunaFGKN <tunafigankaplan496@gmail.com>
2025-11-24 14:38:48 +0300

    refactor: api key değiştirildi

--- app/routers/ai.py
+++ app/routers/ai.py
@@ -6,9 +6,10 @@ from app.db.database import get_db
 from app.models.task import Task
 from app.schemas.ai import AIRequest
 from google import genai
+from app.core.config import settings
 
 # API KEY'i buraya koy veya .env dosyasından çek
-client = genai.Client(api_key="AIzaSyAcWevuPiR0eeEDRpX7IRuZun-6W6i1RdY")
+client = genai.Client(api_key=settings.GOOGLE_API_KEY)
 model = client.models.generate_content(model="gemini-2.5-flash", contents="Explain how AI works in a few words")
 
 router = APIRouter()
